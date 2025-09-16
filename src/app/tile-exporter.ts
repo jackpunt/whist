190d5222213755096715d209a74014d411053ec8
jackpunt <jpeckj@gmail.com>
2025-09-15 19:43:52 -0700

    all the pips

--- src/app/tile-exporter.ts
+++ src/app/tile-exporter.ts
@@ -1,6 +1,5 @@
-import { PageSpec, TileExporter as TileExporterLib, type CountClaz, type GridSpec, type Tile } from "@thegraid/easeljs-lib";
+import { TileExporter as TileExporterLib, type CountClaz, type GridSpec } from "@thegraid/easeljs-lib";
 import { WhistCard } from "./whist-card";
-import type { DisplayObject } from "@thegraid/easeljs-module";
 
 // end imports
 
@@ -23,7 +22,7 @@ export class TileExporter extends TileExporterLib {
     x0: 60 + 1040/2, y0: 25 + 734/2, delx: 1122.5, dely: 734, bleed: 30, bgColor: 'white',
   }
   static fourUp: GridSpec = {
-    width: 2600, height: 3000, nrow: 4, ncol: 3, cardh: 1040, cardw: 734, double: false,
+    width: 2600, height: 3600, nrow: 3, ncol: 3, cardh: 1040, cardw: 734, double: false,
     y0: 60 + 1040/2, x0: 85 + 734/2, dely: 1122.5, delx: 814, bleed: 30, bgColor: 'white',
   }
 
--- src/app/whist-card.ts
+++ src/app/whist-card.ts
@@ -1,4 +1,4 @@
-import { C, F } from "@thegraid/common-lib";
+import { C, F, type XY } from "@thegraid/common-lib";
 import { AliasLoader, CenterText, RectShape, type CountClaz, type Paintable } from "@thegraid/easeljs-lib";
 import { type DisplayObject } from "@thegraid/easeljs-module";
 import { Tile } from "@thegraid/hexlib";
@@ -7,7 +7,7 @@ import { TileExporter } from "./tile-exporter";
 
 // some cards have multiple 'run' boxes! so we allow string | string[]
 type CARD = {
-  Aname: string, color: string,
+  Aname: string, color: string, ranks: string[],
 };
 
 function rgbaToName(v: Uint8ClampedArray, alpha?: number|string) {
@@ -29,13 +29,16 @@ export class WhistCard extends Tile  {
   static get fnames() {
     return WhistCard.cards.flatMap(card => [card.Aname, `Ninja-${card.Aname}`]);
   }
+
+  static ranks = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'K'];
+
   // four suits:
   static cards: CARD[] = [
-    {Aname: 'sword', color: 'white', },
-    {Aname: 'staff', color: 'purple', },
-    {Aname: 'stars', color: 'blue', },
-    {Aname: 'knives', color: 'red', },
-    {Aname: 'arrows', color: 'yellow', },
+    {Aname: 'arrows', color: 'yellow', ranks: ['J'] },
+    {Aname: 'sword', color: 'white', ranks: WhistCard.ranks},
+    {Aname: 'staff', color: 'purple', ranks: WhistCard.ranks, },
+    {Aname: 'stars', color: 'blue', ranks: WhistCard.ranks, },
+    {Aname: 'knives', color: 'red', ranks: WhistCard.ranks, },
   ];
 
   /** color map: canonical name -> hmtl color code */
@@ -56,7 +59,9 @@ export class WhistCard extends Tile  {
   }
 
   static allCards(): CountClaz[] {
-    return WhistCard.cards.map((card: CARD) => [1, WhistCard, card, 'A']);
+    return WhistCard.cards.flatMap((card: CARD) =>
+      card.ranks.map(rank => [1, WhistCard, card, rank]) as CountClaz[]
+    );
   }
 
   override get radius() { return 734 } // nextRadius
@@ -105,15 +110,51 @@ export class WhistCard extends Tile  {
       this.addChild(dobj2);
     }
   }
-
+  suitBitmap(dw = 1, icon = false) {
+    const iwide = this.gridSpec.cardw! * .8;
+    const cname = `${icon ? '' : 'Ninja-'}${this.Aname}`;
+    return AliasLoader.loader.getBitmap(cname, iwide * dw);
+  }
+  // y: -1 or y: -2 --> rotate 180
+  npips(n = 1, dw = .25) {
+    const xyndx = [
+      [{ x: 0, y: 0 }], // 'A' or 'K' or 'J'
+      [{ x: 0, y: 0 }], // 'A' or 'K' or 'J'
+      [{ x: 0, y: -2 }, { x: 0, y: 2 }],
+      [{ x: 1, y: -2 }, { x: -1, y: 2 }, { x: 0, y: 0 }],
+      [{ x: -1, y: -2 }, { x: 1, y: -2 }, { x: 1, y: 2 }, { x: -1, y: 2 }],
+      [{ x: -1, y: -2 }, { x: 1, y: -2 }, { x: 1, y: 2 }, { x: -1, y: 2 }, { x: 0, y: 0 }],
+      [{ x: -1, y: -2 }, { x: -2, y: 0 }, { x: -1, y: 2 }, { x: 1, y: -2 }, { x: 2, y: 0 }, { x: 1, y: 2 }], // 6
+      [{ x: -1, y: -2 }, { x: -2, y: 0 }, { x: -1, y: 2 }, { x: 1, y: -2 }, { x: 2, y: 0 }, { x: 1, y: 2 }, { x: 0, y: -1 }], // 7
+      [{ x: -1, y: -2 }, { x: -2, y: 0 }, { x: -1, y: 2 }, { x: 1, y: -2 }, { x: 2, y: 0 }, { x: 1, y: 2 }, { x: 0, y: -1 }, { x: 0, y: 1 }], // 8
+      [{ x: -1, y: -2 }, { x: -2, y: -1 }, { x: -2, y: 1 }, { x: -1, y: 2 }, { x: 1, y: -2 }, { x: 2, y: -1 }, { x: 2, y: 1 }, { x: 1, y: 2 }, { x: 0, y: 0 }], // 9
+      [{ x: -1, y: -2 }, { x: -2, y: -1 }, { x: -2, y: 1 }, { x: -1, y: 2 }, { x: 1, y: -2 }, { x: 2, y: -1 }, { x: 2, y: 1 }, { x: 1, y: 2 }, { x: 0, y: 2 }, { x: 0, y: -2 }], // 10
+    ] as XY[][];
+    const x1 = .22, x2 = .27;
+    const y2 = .35, y1 = y2 * .33, y10 = y2 * .51, y20 = y2 * .55;
+    const { width, height } = this.getBounds();
+    const xp = (x: number) => [-x2, -x1, 0, x1, x2][x + 2];
+    const ypa = (x: number) => (x == 0) ? [-y20, -y10, 0, y10, y20] : [-y2, -y1, 0, y1, y2];
+    const yp = (x: number, y: number) => ypa(x)[y + 2];
+    const xyp = (n: number) => xyndx[n].map(({ x, y }) => ({ x: xp(x), y: yp(x, y) } as XY));
+    const xyAry = xyp(n);
+    xyAry.forEach(({ x, y }) => {
+      const img = this.suitBitmap(dw);
+      img.x = x * width;
+      img.y = y * height;
+      if (y > 0) img.rotation = 180;
+      this.addChild(img);
+    })
+  }
   addComponents() {
-    //
-    const h = this.gridSpec.cardh!;
-    const cname = `Ninja-${this.Aname}`;
-    const imgWide = this.gridSpec.cardw! * .8;
-    const bmImage = AliasLoader.loader.getBitmap(cname, imgWide); // scaled to fit cardw
+    const dw = (this.rank == 'A' ? .6 : (this.rank == 'K' || this.rank == 'J') ? 1 : .33);
+    const bmImage = this.suitBitmap(dw);
     if (bmImage) {
-      this.addChild(bmImage); // TODO: maybe make 'pips' with smaller image (except K & A)
+      if (dw > .5) {
+        this.addChild(bmImage); // TODO: maybe make 'pips' with smaller image (except K & A)
+      } else {
+        this.npips(Number.parseInt(this.rank), dw)
+      }
     }
     const font = WhistCard.rankFont;
     const { x, y, width, height } = this.getBounds();
@@ -123,11 +164,10 @@ export class WhistCard extends Tile  {
     const rtext2 = new CenterText(this.rank, font, C.BLACK); rtext2.textAlign = 'right';
     this.set2corners(rtext, dx*1.15, rtext.getMeasuredLineHeight() * 1.1, rtext2);
 
-    const sWide = imgWide * .15;
-    const sImage = AliasLoader.loader.getBitmap(this.Aname, sWide);
-    const sImage2 = AliasLoader.loader.getBitmap(this.Aname, sWide);
-    if (sImage) {
-      this.set2corners(sImage, dx - sWide * .15, dy, sImage2);
+    const sImage1 = this.suitBitmap(.15, true);
+    const sImage2 = this.suitBitmap(.15, true);
+    if (sImage1) {
+      this.set2corners(sImage1, dx - 13, dy, sImage2);
     }
     this.paint(this.color)
   }
